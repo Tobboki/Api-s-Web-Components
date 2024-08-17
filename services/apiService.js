d860076a0195bab7a7ee5013a0340e6b36b0b8b5
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-08-17 15:42:02 +0300

    Created services/apiService.js and in it i implemented getUsers, getPosts functions

---
+++ services/apiService.js
@@ -0,0 +1,21 @@
+export async function getUsers() {
+  try {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getPosts() {
+  try {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
